01447dbf7e8bf9fd9b4e6b8346d0c2657cbc4195
Elisea <tokiniaina.andriamanga@gmail.com>
2026-08-23 22:15:00 +0300

    correction du bug de model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ use App\Models\Mouvement;
 use App\Models\Provider;
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+class Product extends Model
 {
     //
     protected $fillable = [
